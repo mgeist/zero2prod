931957fcf33d73366b34cbb750131d4b8d0dca73
Mat Geist <matgeist@gmail.com>
2021-11-10 16:42:09 -0700

    Refactor project structure

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,14 @@
 use std::net::TcpListener;
 
+use zero2prod::startup;
+
 /// Spin up an instance of our application
 /// and return its address (http://localhost:XXXX)
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
     format!("http://127.0.0.1:{}", port)

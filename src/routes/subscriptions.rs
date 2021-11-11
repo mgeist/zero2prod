e2ecbe2b70e42b2792f9e835d2c9780270a42081
Mat Geist <matgeist@gmail.com>
2021-11-11 16:19:44 -0700

    Add telemetry / tracing / logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -9,8 +9,28 @@ pub struct FormData {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Adding a new subscriber",
+    skip(form, pool),
+    fields (
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    )
+)]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> impl Responder {
-    match sqlx::query!(
+    match insert_subscriber(&form, &pool).await {
+        Ok(_) => HttpResponse::Ok(),
+        Err(_) => HttpResponse::InternalServerError(),
+    }
+    .await
+}
+
+#[tracing::instrument(
+    name = "Saving new subscriber details in the database",
+    skip(form, pool)
+)]
+pub async fn insert_subscriber(form: &FormData, pool: &PgPool) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
@@ -20,13 +40,11 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> im
         form.name,
         Utc::now()
     )
-    .execute(pool.get_ref())
+    .execute(pool)
     .await
-    {
-        Ok(_) => HttpResponse::Ok(),
-        Err(e) => {
-            println!("Failed to execute query: {}", e);
-            HttpResponse::InternalServerError()
-        }
-    }
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,20 @@
 use std::net::TcpListener;
 
 use actix_web::dev::Server;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::{health_check, subscribe};
 
 pub fn run(listener: TcpListener, pool: PgPool) -> Result<Server, std::io::Error> {
-    let pool = web::Data::new(pool);
+    let pool = Data::new(pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
-            .route("subscriptions", web::post().to(subscribe))
+            .route("/subscriptions", web::post().to(subscribe))
             .app_data(pool.clone())
     })
     .listen(listener)?
